b7ae7c0cbc839a318e1c49fae4a21244660a5ea3
rafa-houssam <rafahoussam4edu@gmail.com>
2025-06-09 10:53:46 +0100

    installing the themes

--- base/views.py
+++ base/views.py
@@ -56,8 +56,10 @@ def CreateRoom(request):
     if(request.method=='POST'):
         form=RoomForm(request.POST)
         if(form.is_valid()):
-            form.save()
-            return redirect('home')
+           room= form.save(commit=False)
+           room.host=request.user
+           room.save()
+           return redirect('home')
     context={'form':form}
     return render(request,'base/room_form.html',context)
 @login_required(login_url='login')
