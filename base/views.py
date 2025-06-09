f680dcff9c9fa0157761960314140831eaf6567d
rafa-houssam <rafahoussam4edu@gmail.com>
2025-06-10 00:20:55 +0100

    completed theme installation

--- base/views.py
+++ base/views.py
@@ -56,27 +56,34 @@ def CreateRoom(request):
     topics=Topic.objects.all()
     if(request.method=='POST'):
         topic_name=request.POST.get('topic')
-        form=RoomForm(request.POST)
-        if(form.is_valid()):
-           room= form.save(commit=False)
-           room.host=request.user
-           room.save()
-           return redirect('home')
+        topic,created=Topic.objects.get_or_create(name=topic_name)
+        Room.objects.create(
+            host=request.user,
+            topic=topic,
+            name=request.POST.get('name'),
+            description=request.POST.get('description')
+        )
+
+        return redirect('home')
     context={'form':form,'topics':topics}
     return render(request,'base/room_form.html',context)
 @login_required(login_url='login')
 def UpdateRoom(request,pk):
     room=Room.objects.get(id=pk)
+    
     form=RoomForm(instance=room)
     topics=Topic.objects.all()
     if(room.host!=request.user):
         return HttpResponse("you're not allowed here my friend ")
     if(request.method=='POST'):
-        form=RoomForm(request.POST,instance=room)
-        if(form.is_valid()):
-            form.save()
-            return redirect('home')
-    context={'form':form,'topics':topics}
+        topic_name=request.POST.get('topic')
+        topic,created=Topic.objects.get_or_create(name=topic_name)
+        room.name=request.POST.get('name')
+        room.topic=topic
+        room.description=request.POST.get('description')
+        room.save()
+        return redirect('home')
+    context={'form':form,'topics':topics,'room':room}
     return render(request,'base/room_form.html',context)
 
 
