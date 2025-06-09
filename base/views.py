52d2c92f3b94627f7d06ccbbb39cc4508a5ab3f2
rafa-houssam <rafahoussam4edu@gmail.com>
2025-06-09 21:52:03 +0100

    room updating

--- base/views.py
+++ base/views.py
@@ -53,6 +53,7 @@ def room(request, pk):
 @login_required(login_url='login')
 def CreateRoom(request):
     form=RoomForm()
+    topics=Topic.objects.all()
     if(request.method=='POST'):
         form=RoomForm(request.POST)
         if(form.is_valid()):
@@ -60,12 +61,13 @@ def CreateRoom(request):
            room.host=request.user
            room.save()
            return redirect('home')
-    context={'form':form}
+    context={'form':form,'topics':topics}
     return render(request,'base/room_form.html',context)
 @login_required(login_url='login')
 def UpdateRoom(request,pk):
     room=Room.objects.get(id=pk)
     form=RoomForm(instance=room)
+    topics=Topic.objects.all()
     if(room.host!=request.user):
         return HttpResponse("you're not allowed here my friend ")
     if(request.method=='POST'):
@@ -73,7 +75,7 @@ def UpdateRoom(request,pk):
         if(form.is_valid()):
             form.save()
             return redirect('home')
-    context={'form':form}
+    context={'form':form,'topics':topics}
     return render(request,'base/room_form.html',context)
 
 
