355a54cffedc3fa9f7f04be766d3b95c2d3510cf
rafa-houssam <rafahoussam4edu@gmail.com>
2025-06-10 10:36:06 +0100

    user editing profile

--- base/views.py
+++ base/views.py
@@ -157,4 +157,8 @@ def userProfile(request,pk):
     context={'user':user,'rooms':rooms,'room_messages':room_messages,'topics':topics}
     return render(request,'base/profile.html',context)
 
+@login_required(login_url='login')
+def updateUser(request):
+    context={}
+    return render(request,'base/update-user.html',context)
     
